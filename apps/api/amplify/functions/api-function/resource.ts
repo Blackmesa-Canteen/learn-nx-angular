db62ece3c97d759ba030c7c71c4859890a2ba21a
Xiaotian Li <xiaotian.li@lapisit.com.au>
2025-11-17 15:50:11 +1100

    feat: update amplify location

--- apps/api/amplify/functions/api-function/resource.ts
+++ apps/api/amplify/functions/api-function/resource.ts
@@ -1,8 +1,7 @@
 import { defineFunction } from '@aws-amplify/backend';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export const coreApiFunction = defineFunction({
   name: "learnCoreApiFunction",
   runtime: 22,
-  entry: 'apps/api/src/main.serverless.ts',
+  entry: '../../../src/main.serverless.ts',
 });
