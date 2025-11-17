2d82d33da83eb814f0161d26f4c0c73fa7d54b02
Xiaotian Li <xiaotian.li@lapisit.com.au>
2025-11-17 12:04:24 +1100

    feat: fix module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BookModule } from './book.module';
+import { BookModule } from './book/book.module';
 
 @Module({
   imports: [BookModule],
